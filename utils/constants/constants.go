fb2479afae60952f0184051539e4100ae504b19e
Joshua Przybyszewski <josh.prezby@gmail.com>
2018-01-15 21:20:09 -0600

    Remove unnecessary private vars
    because they exist where they need to now

--- utils/constants/constants.go
+++ utils/constants/constants.go
@@ -14,31 +14,9 @@ const (
 	EmptyTile     = types.Tile(0)
 	EmptyTileChar = `.`
 
-	onePresence   = types.Presence(1 << uint(1-1))
-	twoPresence   = types.Presence(1 << uint(2-1))
-	threePresence = types.Presence(1 << uint(3-1))
-	fourPresence  = types.Presence(1 << uint(4-1))
-	fivePresence  = types.Presence(1 << uint(5-1))
-	sixPresence   = types.Presence(1 << uint(6-1))
-	sevenPresence = types.Presence(1 << uint(7-1))
-	eightPresence = types.Presence(1 << uint(8-1))
-	ninePresence  = types.Presence(1 << uint(9-1))
-
 	EmptyPuzzle = `.................................................................................`
 )
 
 var (
 	AllEntries       = []types.Entry{1, 2, 3, 4, 5, 6, 7, 8, 9}
-
-	allEntriesPresence = []types.Presence{
-		onePresence,
-		twoPresence,
-		threePresence,
-		fourPresence,
-		fivePresence,
-		sixPresence,
-		sevenPresence,
-		eightPresence,
-		ninePresence,
-	}
 )
