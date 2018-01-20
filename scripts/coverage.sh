acd2f20e857bc779049d877a94fc1b641310d99c
prezby <prezby@iastate.edu>
2018-01-20 17:30:16 -0600

    ignore testing on vendor directories!

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -7,11 +7,11 @@ set -e
 
 echo "mode: atomic" > coverage.txt
 
-for d in $(find ./* -maxdepth 10 -type d); do
+for d in $(find ./* -maxdepth 10 -type d -not -path "*vendor*"); do
     if ls $d/*.go &> /dev/null; then
-        go test  -coverprofile=profile.out -covermode=atomic $d
+        go test -coverprofile=profile.out -covermode=atomic $d
         if [ -f profile.out ]; then
-            echo "$(pwd)"
+            echo "$(pwd) FINISHED testing $d"
             cat profile.out | grep -v "mode: " >> coverage.txt
             rm profile.out
         fi
