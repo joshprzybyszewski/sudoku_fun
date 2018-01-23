50ceaf5d70d5f5ecf307086d1c0cc5f82e1b7b5c
prezby <prezby@iastate.edu>
2018-01-23 15:19:48 -0600

    sry matt im ripping you apart
    but this is my repo and i want it my way

--- twodee/matt/solver.go
+++ twodee/matt/solver.go
@@ -1,75 +1,16 @@
 package matt
 
 import (
-	"bufio"
 	"errors"
-	"flag"
 	"fmt"
-	"io"
-	"log"
-	"os"
 	"strconv"
 	"time"
-
-	utils "github.com/joshprzybyszewski/sudoku_fun/utils/matt"
 
 	"github.com/joshprzybyszewski/sudoku_fun/twodee"
 	joshUtils "github.com/joshprzybyszewski/sudoku_fun/utils"
 	"github.com/joshprzybyszewski/sudoku_fun/utils/constants"
+	utils "github.com/joshprzybyszewski/sudoku_fun/utils/matt"
 )
-
-var (
-	filePath = flag.String(`input`, `files/input.sk`, `path of file containing puzzles`)
-)
-
-func peaceOut(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
-func whatusedtobemain() {
-	flag.Parse()
-
-	fp, err := os.Open(*filePath)
-	peaceOut(err)
-
-	bfp := bufio.NewReader(fp)
-	for {
-		line, _, err := bfp.ReadLine()
-		if err == io.EOF {
-			break
-		}
-		peaceOut(err)
-
-		board := utils.Board{}
-		for i, b := range []rune(string(line)) {
-			switch b {
-			case '1':
-				board[i/9][i%9] = 1
-			case '2':
-				board[i/9][i%9] = 2
-			case '3':
-				board[i/9][i%9] = 3
-			case '4':
-				board[i/9][i%9] = 4
-			case '5':
-				board[i/9][i%9] = 5
-			case '6':
-				board[i/9][i%9] = 6
-			case '7':
-				board[i/9][i%9] = 7
-			case '8':
-				board[i/9][i%9] = 8
-			case '9':
-				board[i/9][i%9] = 9
-			}
-		}
-		t0 := time.Now()
-		_, attempts := utils.Solve(board)
-		log.Printf("Solved in %v after %d attempts", time.Since(t0), attempts)
-	}
-}
 
 func readPuzzle(line string) (*utils.Board, error) {
 	board := utils.Board{}
@@ -108,8 +49,7 @@ type BoardReader func(str string) (p *utils.Board, err error)
 func PuzzleSolver(_ twodee.SudokuReader, sudokuPuzzle string) (*time.Duration, int, string, error) {
 	board, err := readPuzzle(sudokuPuzzle)
 	if err != nil {
-		println(`BAD SUDOKU!!`)
-		return nil, 0, ``, nil
+		return nil, -1, ``, errors.New(fmt.Sprintf("Bad Sudoku: %v", err.Error()))
 	}
 
 	t0 := time.Now()
@@ -117,16 +57,13 @@ func PuzzleSolver(_ twodee.SudokuReader, sudokuPuzzle string) (*time.Duration, i
 	duration := time.Since(t0)
 
 	if err != nil {
-		println(fmt.Sprintf("COULDN'T SOLVE: %v", err.Error()))
-		return nil, -1, constants.EmptyPuzzle, nil
+		return nil, -1, constants.EmptyPuzzle, errors.New(fmt.Sprintf("Couldn't Solve: %v", err.Error()))
 	}
 
 	solutionStr := toString(&solvedBoard)
 
 	if !joshUtils.BruteForceCheck(solutionStr) {
-		println(solutionStr)
-		println(`WARNING IT DIDN'T ACTUALLY SOLVE'`)
-		return nil, -1, constants.EmptyPuzzle, nil
+		return nil, -1, constants.EmptyPuzzle, errors.New(fmt.Sprintf("We thought it solved, but it didn't!\n%v", solutionStr))
 	}
 
 	return &duration, int(attempts), solutionStr, nil
@@ -139,7 +76,6 @@ func toString(board *utils.Board) string {
 			thechar := strconv.Itoa(board[r][c])
 			str += thechar
 		}
-
 	}
 
 	return str
